d4f85d8a52d3458f0a2f7260729b62245e6b7a99
alt277 <altima875@gmail.com>
2021-01-16 18:27:00 +0300

    Init commit

---
+++ shop-picture-service/src/main/java/ru/geekbrains/service/PictureService.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.service;
+
+import ru.geekbrains.persist.model.PictureData;
+import ru.geekbrains.persist.model.Product;
+
+import java.util.Optional;
+
+public interface PictureService {
+
+    Optional<String> getPictureContentTypeById(long id);
+
+    Optional<byte[]> getPictureDataById(long id);
+
+    PictureData createPictureData(byte[] picture);
+
+    Optional<Product> getProductByPictureId(long id);
+
+    // TODO перенести сюда функционал получения списка картинок
+
+    // TODO перенести сюда функционал для удаления картинок
+    void removePicture(long id);
+}
